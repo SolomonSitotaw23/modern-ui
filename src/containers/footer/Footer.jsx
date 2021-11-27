a7d881620d3f5685fe75412c5a6707ce593cdac2
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2021-11-27 14:18:02 +0300

    footer logo

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -13,13 +13,31 @@ const Footer = () => {
         <p>Request Early Access</p>
       </div>
       <div className="gpt3__footer-links">
-        <div className="gpt3__footer-liks_logo">
+        <div className="gpt3__footer-links_logo">
           <img src={gpt3Logo} alt="gpt3Logo" />
           <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
         <div className="gpt3__footer-links_div">
-            <h4>Links</h4>
-            <p></p>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+        <div className="gpt3__footer-copright">
+            <p>© 2021 GPT-3. All rights reserved.</p>
         </div>
       </div>
     </div>
