b54ad8b98e897beeb6d086d2eb95acb5c8fac011
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2021-11-27 13:47:36 +0300

    footer start

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import gpt3Logo from "../../assets/logo.svg";
+import "./footer.css";
 const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-liks_logo">
+          <img src={gpt3Logo} alt="gpt3Logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
-    )
-}
+        <div className="gpt3__footer-links_div">
+            <h4>Links</h4>
+            <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
